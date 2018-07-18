dddcf91e1d068398d6c9655dfdc726873267b799
toanprox7 <37140179+toanprox7@users.noreply.github.com>
2018-07-18 11:51:39 +0700

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,6 +1,5 @@
 var sequelize = require('sequelize');
-var db = new sequelize('postgres://fcxctktjmxxtdj:9ce4a0d0579f74d3c6fd9ea94985b382fa4de715c0caa1ce1ac1d85767c4852c@ec2-54-227-240-7.compute-1.amazonaws.com:5432/db4np76m4rj0n2
-');
+var db = new sequelize('postgres://fcxctktjmxxtdj:9ce4a0d0579f74d3c6fd9ea94985b382fa4de715c0caa1ce1ac1d85767c4852c@ec2-54-227-240-7.compute-1.amazonaws.com:5432/db4np76m4rj0n2');
 var User = db.define('users',{
     username:sequelize.STRING,
     email:sequelize.STRING,
